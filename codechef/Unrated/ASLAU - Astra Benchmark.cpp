95a5add71451b72284a2177080d2a88e20fc93a5
Aditya raj prasad <prasad987aditya@gmail.com>
2026-09-07 20:01:42 +0530

    Add Astra Benchmark (Unrated)

---
+++ codechef/Unrated/ASLAU - Astra Benchmark.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a , b, x;
+    std::cin >> a>>b>>x;
+    if((a+b>=x)) std::cout << "YES" << std::endl;
+    else std::cout << "NO" << std::endl;
+}
